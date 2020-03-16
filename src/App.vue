ca5a7fa0ba09db8875b991dc7970a350abd5d0ec
rome18 <romanos.albert.adhitya@gmail.com>
2020-03-16 14:34:42 +0700

    Whole new folder

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="app">
+  <p>{{ fullname }}</p>
+  <input v-model="fullname">
+</div>
+</template>
+
+<script>
+  var app = new Vue({
+    el: '#app',
+    data: {
+      fullname: 'Your Full Name!'
+    }
+  })
+</script>
+
