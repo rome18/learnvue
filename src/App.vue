d3b857d0a1c22821476ff717b4464ca2cda38150
rome18 <romanos.albert.adhitya@gmail.com>
2020-03-17 11:33:49 +0700

    Training1: Name, Math, Images, input_text

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    <div id="app">
-      <h1 v-once> {{ judul }} </h1>
-      <p>{{respon}}</p>
-      <input type="text" v-on:input="changeTitle">
-      <p>{{ sayHello() }} - <a v-bind:href="link">Google</a></p>
+  <div id="exercise">
+    <p> VueJS is pretty cool - {{ name }} {{ age }}</p>
+    <p>My Age times 3 is {{ age * 3 }}</p>
+    <p>A Random Number between 0 to 1 {{ random() }}</p>
+    <div>
+      <img v-bind:src="image" style="width:100px;height:100px">
+    </div>
+    <div>
+      <input type="text" v-bind:value="name">
     </div>
   </div>
 </template>
@@ -12,17 +15,13 @@
 <script>
 export default {
   data:()=>({
-    judul:'Hello World!',
-    respon: 'Hiii!',
-    link: 'http://google.com'
-  }), 
+    name: 'Romanos Albert Adhitya',
+    age: 23,
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fimages%2Fanimals%2Fcow&psig=AOvVaw2h5--JgQiV-Za-JgXX5_W9&ust=1584505495591000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIjTzYzVoOgCFQAAAAAdAAAAABAD'
+  }),
   methods: {
-    changeTitle: function(event) {
-      this.judul = event.target.value;
-    },
-    sayHello: function(){
-      this.judul = 'Hi Deski';
-      return this.judul;
+    random:function() {
+      return Math.random();
     }
   }
 }
