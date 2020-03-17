5ab20be7f881a9309bd4415730b0830cc28532e8
rome18 <romanos.albert.adhitya@gmail.com>
2020-03-17 10:49:25 +0700

    Title, reactivity, href

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-<div id="app">
-  <p>{{ fullname }}</p>
-  <input v-model="fullname">
-</div>
+  <div>
+    <div id="app">
+      <h1 v-once> {{ judul }} </h1>
+      <p>{{respon}}</p>
+      <input type="text" v-on:input="changeTitle">
+      <p>{{ sayHello() }} - <a v-bind:href="link">Google</a></p>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default{
-    data:()=>({
-        fullname: 'Your Full Name!'
-    })
+export default {
+  data:()=>({
+    judul:'Hello World!',
+    respon: 'Hiii!',
+    link: 'http://google.com'
+  }), 
+  methods: {
+    changeTitle: function(event) {
+      this.judul = event.target.value;
+    },
+    sayHello: function(){
+      this.judul = 'Hi Deski';
+      return this.judul;
+    }
   }
+}
 </script>
 
